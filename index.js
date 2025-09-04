1906e4cd8960289e2f5eedfa50340e181c1384db
shahidulllah <mdshahidsumon177@gmail.com>
2025-09-04 20:47:16 +0600

    jwt api creation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express from "express";
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 
 dotenv.config();
 const app = express();
@@ -11,9 +12,19 @@ app.use(cors());
 app.use(express.json());
 
 //auth api
-app.post('/jwt', async (req, res) => {
-  
-})
+app.post("/jwt", async (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.JWT_SECRET, {
+    expiresIn: "5h",
+  });
+
+  res
+    .cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+    })
+    .send({ success: true });
+});
 
 app.get("/", async (req, res) => {
   res.send("Concept practice server is running");
