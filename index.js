b75dd87af91709b6985109d8e39996e66f136f7b
shahidulllah <mdshahidsumon177@gmail.com>
2025-09-07 10:34:32 +0600

    token verify

--- index.js
+++ index.js
@@ -18,6 +18,23 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+//Token verify
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access." });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized accesss" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
 //auth api
 app.post("/jwt", async (req, res) => {
   const user = req.body;
